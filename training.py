cd2c8d02234d3225c8805c75f4c0679300328b83
AnjaliAnna <anjaliannapeter4@gmail.com>
2021-11-16 22:41:37 +0530

    training.py update documentation

--- training.py
+++ training.py
@@ -1,15 +1,15 @@
 # preprocessing and training
 import cv2  # computer vision library for image processing
-import math 
-import matplotlib.pyplot as plt # for plotting or plot generation
-import pandas as pd # allows importing data from various file formats like csv
-import tensorflow as tf # for building model
+import math
+import matplotlib.pyplot as plt  # for plotting or plot generation
+import pandas as pd  # allows importing data from various file formats like csv
+import tensorflow as tf  # for building model
 from keras.preprocessing import image
-import numpy as np
+import numpy as np  # library with tools supporting large multi dimensional arrays
 from keras.utils import np_utils
 from skimage.transform import resize
 
-dataf = pd.read_csv(image.csv) #imports a csv file to dataframe format
+dataf = pd.read_csv(image.csv)  # imports a csv file to dataframe format
 print(dataf.head(5))
 x = []
 print("hello")
